78aa4fa6c3702ac4b0c9085a38cd981cd0c54f99
David Silva <davidlsilva.sp@gmail.com>
2023-01-20 09:30:04 -0300

    feat: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,18 @@
-export default function Header() {
+import Link from "next/link";
+import Image from "next/image";
+import { Box } from "@chakra-ui/react";
+import common from "../../styles/common.module.scss";
+import styles from "./header.module.scss";
+
+export default function Header(): JSX.Element {
   // TODO
+  return (
+    <div className={styles.header}>
+      <Box maxWidth={1280} p={5} margin="auto">
+        <Link href="/">
+          <Image src="/logo.svg" alt="logo" width={240} height={105} />
+        </Link>
+      </Box>
+    </div>
+  );
 }
